4876bcb649079d4056d68c3cbae93c6dc7fff08c
Gilbert Acosta <acostagilberto95@gmail.com>
2020-12-04 22:56:11 -0400

    Fetch API

---
+++ 02-intro-javascript/src/bases/10-fetch.js
@@ -0,0 +1,17 @@
+
+const apiKey = 'YoMlNMOQn17UR2r2dlCxgWEnSnKo6sK5';
+
+const peticion = fetch(
+  `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+);
+
+peticion.then( resp => resp.json())
+        .then( ({ data } )=> {
+              const { url } = data.images.original;
+              const img = document.createElement('img');
+              img.src = url;
+
+              document.body.append(img);
+              
+    
+        }).catch(console.warn);
