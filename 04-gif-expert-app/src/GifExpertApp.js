46fa75af5a546d8fe8f27d1b85e91790e22c318c
Gilbert Acosta <acostagilberto95@gmail.com>
2020-12-28 23:24:12 -0400

    seguir en useEffect

---
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,33 @@
+import React, { Fragment, useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = () => {
+
+
+  const [categories, setCategories] = useState(['Dragon Ball']);
+  
+  // const handleAdd = () => {
+
+  //   setCategories([...categories, 'HunterXHunter']);
+  //   setCategories(cats => [ ...cats, 'HunterXHunter']);
+  // };
+
+  return (
+		<Fragment>
+			<h2>GifExpertApp</h2>
+			<AddCategory setCategories={setCategories} />
+			<hr />
+
+			<ol>
+				{categories.map((category, i) => (
+					<GifGrid key={category} category={category} />
+				))}
+			</ol>
+		</Fragment>
+	);
+
+};
+
+
+export default GifExpertApp;
