3561e8cd8f22ebbbafd07b033a999495532eb80c
Gilbert Acosta <acostagilberto95@gmail.com>
2021-01-10 12:33:00 -0400

    seguir pruebas

---
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme'
+import { AddCategory } from '../../components/AddCategory';
+
+
+describe('Pruebas en <AddCategory />', () => {
+
+  const setCategories = () => {};
+
+  test('debe de mostrarse correctamente', () => {
+
+    const wrapper = shallow(<AddCategory setCategories={ setCategories } />); 
+    expect( wrapper ).toMatchSnapshot();
+    
+  });
+});
--- 04-gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -4,15 +4,44 @@ import { GifGridItem } from '../../components/GifGridItem';
 
 
 describe('Prueba en <GifGridItem />', () => {
+  const title = 'Dragon Ball Z';
+  const url = 'https://localhost/algo.jpg';
+  const wrapper = shallow(<GifGridItem title={ title } url={ url } />);
 
   test('debe de mostrar el componente correctamente ', () => {
 
-    const title = 'Dragon Ball Z';
-    const url = 'https://localhost/algo.jpg';
-    const wrapper = shallow(<GifGridItem title={ title } url={ url } />);
-
+    
     expect( wrapper ).toMatchSnapshot();
     
   });
+
+  test('debe de tener un parrafo con el title', () => {
+    
+
+    const p = wrapper.find('p');
+
+    expect( p.text().trim() ).toBe( title );
+  });
+
+  test('debe de tenr la imagen igual al url y alt de los props', () => {
+
+    const img = wrapper.find('img');
+    // console.log(img.props('src'));
+    expect(img.prop('src')).toBe( url );
+    expect(img.prop('alt')).toBe( title );
+    
+    
+  });
+
+  test('debe tener animate__fadeIn', () => {
+    const div = wrapper.find('div');
+    
+    const className = div.prop('className');
+
+    expect(className.includes('animate__fadeIn')).toBe( true );
+    
+    
+  });
+  
   
 });
