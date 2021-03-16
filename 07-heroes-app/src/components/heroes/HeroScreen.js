539b9fcd84c89b2dafc6bbe64cde1121397ff246
Gilbert Acosta <gisadeveloper@gmail.com>
2021-03-16 06:52:09 -0400

    Seguir en SearchComponent

--- 07-heroes-app/src/components/heroes/HeroScreen.js
+++ 07-heroes-app/src/components/heroes/HeroScreen.js
@@ -1,30 +1,57 @@
-import React from 'react';
-import { Redirect, useParams } from 'react-router';
-import { getHeroesById } from '../../selectors/getHeroesById';
+import React, {useMemo} from 'react';
+import {Redirect, useParams} from 'react-router';
+import {getHeroesById} from '../../selectors/getHeroesById';
 
+export const HeroScreen = ({history}) => {
+	const {heroeId} = useParams();
 
+	const hero = useMemo(() => getHeroesById(heroeId), [heroeId]);
 
-export const HeroScreen = () => {
+	if (!hero) {
+		return <Redirect to="/" />;
+	}
 
-  const {heroeId} = useParams();
-  
-  const hero = getHeroesById( heroeId );
+	const handleReturn = () => {
+		if (history.length <= 2) {
+			history.push('/');
+		} else {
+			history.goBack();
+		}
+	};
 
-  if ( !hero ) {
-    return <Redirect to="/"/>
-  }
+	const {superhero, publisher, alter_ego, first_appearance, characters} = hero;
 
-  const {
-    superhero,
-    publisher,
-    alter_ego,
-    first_appearance,
-    characters
-  } = hero;
+	return (
+		<div className="row mt-5">
+			<div className="col-4">
+				<img
+					src={`../assets/${heroeId}.jpg`}
+					alt={superhero}
+					className="img-thumbnail animate__animated animate__fadeInLeft"
+				/>
+			</div>
 
-  return (
-    <div>
-      <h1>HeroScreen</h1>
-    </div>
-  )
-}
+			<div className="col-8 animate__animated animate__fadeIn">
+				<h3>{ superhero }</h3>
+				<ul className="list-group list-group-flush">
+					<li class="list-group-item">
+						<b>Alter ego:</b> {alter_ego}{' '}
+					</li>
+					<li class="list-group-item">
+						<b>Publisher:</b> {publisher}{' '}
+					</li>
+					<li class="list-group-item">
+						<b>Firt appearance:</b> {first_appearance}{' '}
+					</li>
+				</ul>
+
+				<h5> Characters </h5>
+				<p> {characters}</p>
+
+				<button className="btn btn-outline-info" onClick={handleReturn}>
+					Return
+				</button>
+			</div>
+		</div>
+	);
+};
